411beffa9643c0ef4553fcf9dce0c906fd855be4
lithid <long.jeremie@gmail.com>
2012-05-17 08:26:45 -0400

    License update http://sam.zoy.org/wtfpl/

--- src/prog/chkPackages.py
+++ src/prog/chkPackages.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+
+#   CMCompiler is free software. It comes without any warranty, to
+#   the extent permitted by applicable law. You can redistribute it
+#   and/or modify it under the terms of the Do What The Fuck You Want
+#   To Public License, Version 2, as published by Sam Hocevar. See
+#   http://sam.zoy.org/wtfpl/COPYING for more details.
+
 from helper import *
 
 gtk.threads_init()
